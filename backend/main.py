b42e0c7b6cdedd885e284443ef71d24e171e6dd6
yeemans <arthur.roth.07@gmail.com>
2026-03-19 11:14:03 -0700

    added testing url to verify text extraction works

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import requests
+from bs4 import BeautifulSoup
 
 app = FastAPI()
 
@@ -10,7 +12,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def extract_text(url):
+    extracted_text = ""
+    response = requests.get(url)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    paragraphs = soup.find_all('p')
+    
+    for p in paragraphs:
+        extracted_text += p.get_text(strip=True)
+
+    return extracted_text
+
 @app.get("/")
 def root():
-    return {"status": "ok"}
+    test_url = "https://losgatan.com/cats-hill-50th-anniversary-this-saturday/"
+    return {"status": "ok", "testing_text": extract_text(test_url)}
+
+
 
